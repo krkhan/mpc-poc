7d9ffc3394dd097c5209642c1db96561e06a6925
Kamran Khan <k.khan@salesforce.com>
2022-03-26 15:41:09 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Yubico.Core.Iso7816;
+using Yubico.YubiKey;
+using Yubico.YubiKey.Piv;
+using Yubico.YubiKey.Piv.Commands;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        IYubiKeyDevice? yk = SelectYubiKey();
+        if (yk == null)
+        {
+            System.Environment.Exit(1);
+        }
+        Console.WriteLine (yk);
+        using (var piv = new PivSession(yk))
+        {
+            byte[] data = { 0xde, 0xad, 0xbe, 0xef };
+            PutDataCommand putDataCommand =
+                new PutDataCommand(PivDataTag.Retired1, data);
+            PutDataResponse putDataResponse =
+                piv.Connection.SendCommand(putDataCommand);
+
+            if (putDataResponse.StatusWord != SWConstants.Success)
+            {
+                Console.WriteLine("error putting data");
+            }
+        }
+    }
+
+    static IYubiKeyDevice? SelectYubiKey()
+    {
+        IEnumerable<IYubiKeyDevice> list = YubiKeyDevice.FindAll();
+        return list.First();
+    }
+}
